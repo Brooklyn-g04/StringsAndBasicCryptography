2a0edf361b052b3579fd64f22417e1b9d00e1bc8
100103067 <100103067@lubbockisd.net>
2019-10-24 15:27:17 -0500

    Messing with strings and stuff

--- strings and basic cryptography.py
+++ strings and basic cryptography.py
@@ -6,5 +6,22 @@ firstName = "Wilma"
 lastName = "Flintstone"
 
 
-print(firstName + lastName)
+print(firstName + " " + lastName)
 
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
+
+print(name)
+print(lastFirst)
+
+# Repetition
+#  repetition operator: *
+print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row,"*3 + "your boat")
+    print("Gently down the stream")
+    print("Merrily,"*4)
+    print("Life is but a dream")
+
+rowYourBoat()
