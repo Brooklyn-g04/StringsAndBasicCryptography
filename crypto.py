f85325d4b36d94bac67580b8715598c40a2a354d
100103067 <100103067@lubbockisd.net>
2019-10-29 15:46:40 -0500

    10/29 crypto messages and scrambled

--- crypto.py
+++ crypto.py
@@ -2,8 +2,8 @@
 
 # encryption function
 def scramble2Encrypt(plainText):
-    evenChars = ""
-    oddChars = ""
+    evenChars = " "
+    oddChars = " "
     charCount = 0
     for ch in plainText:
         if charCount % 2 == 0:
@@ -14,5 +14,18 @@ def scramble2Encrypt(plainText):
     cipherText = oddChars + evenChars
     return cipherText
 
+def scramble2Decrypt(cipherText):
+    halfLength = len(cipherText)// 2
+    evenChars = cipherText[halfLength:]  # halfLength to the end
+    oddChars = cipherText[:halfLength]  # 0 to halfLength -1
+    plainText = " "
 
+    for i in range(halfLength):
+        plainText = plainText + evenChars[i]
+        plainText = plainText + oddChars[i]
+
+    if len(oddChars) < len(evenChars):
+        plainText = plainText + evenChars[-1]
+
+    return plainText
 
